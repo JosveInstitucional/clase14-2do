f0d955b02286ef5dbe25941aa4d674f5aa48c59b
Hebert Flores <2017110474@untels.edu.pe>
2020-08-03 12:32:31 -0500

    agregar modelo y tabla

--- src/java/Modelo/Ventas.java
+++ src/java/Modelo/Ventas.java
@@ -10,5 +10,81 @@ package Modelo;
  * @author USER
  */
 public class Ventas {
-    
+    private int idventas;
+    private String nombre;
+    private double ganancias;
+    private String vendedor;
+    private double capital;
+
+    /**
+     * @return the idventas
+     */
+    public int getIdventas() {
+        return idventas;
+    }
+
+    /**
+     * @param idventas the idventas to set
+     */
+    public void setIdventas(int idventas) {
+        this.idventas = idventas;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the ganancias
+     */
+    public double getGanancias() {
+        return ganancias;
+    }
+
+    /**
+     * @param ganancias the ganancias to set
+     */
+    public void setGanancias(double ganancias) {
+        this.ganancias = ganancias;
+    }
+
+    /**
+     * @return the vendedor
+     */
+    public String getVendedor() {
+        return vendedor;
+    }
+
+    /**
+     * @param vendedor the vendedor to set
+     */
+    public void setVendedor(String vendedor) {
+        this.vendedor = vendedor;
+    }
+
+    /**
+     * @return the capital
+     */
+    public double getCapital() {
+        return capital;
+    }
+
+    /**
+     * @param capital the capital to set
+     */
+    public void setCapital(double capital) {
+        this.capital = capital;
+    }
 }
+
+
